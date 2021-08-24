52fc1beb309c3acc59e58aa8151dad10b94c0398
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-24 14:44:59 -0700

    moved dnd component out to app

--- client/components/Terminal.jsx
+++ client/components/Terminal.jsx
@@ -2,22 +2,13 @@ import React from 'react';
 import { Rnd } from 'react-rnd';
 import WorkspaceModuleWrapper from './WorkspaceModuleWrapper';
 
-export default function Terminal() {
+export default function Terminal({ currentProblem }) {
   return (
-    <Rnd
-      default={{
-        x: 900,
-        y: 900,
-        width: '40vw',
-        height: '20vh',
-      }}
-      minWidth={500}
-      minHeight={190}
-      bounds="window"
+    <WorkspaceModuleWrapper
+      moduleName="terminal"
+      problemName={currentProblem.name}
     >
-      <WorkspaceModuleWrapper>
-        <h1>terminal</h1>
-      </WorkspaceModuleWrapper>
-    </Rnd>
+      <h1>terminal</h1>
+    </WorkspaceModuleWrapper>
   );
 }
