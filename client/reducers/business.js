2586580f8f638b0447ea0283ac9f8dadad1b559c
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-24 21:24:19 -0700

    make another default problem for testing

--- client/reducers/business.js
+++ client/reducers/business.js
@@ -22,6 +22,26 @@ const initialState = {
         },
       ],
     },
+    {
+      id: 1,
+      name: 'twoSum',
+      description:
+        'This is the default view.  You can use this to play around with code.  If you want to create a problem to save and practice later head to the problems menu above. Taiyaki etsy poutine heirloom small batch drinking vinegar typewriter, four loko chartreuse thundercats. Flannel salvia actually, sriracha gluten-free cloud bread chambray +1. Brunch salvia taxidermy, synth stumptown palo santo four dollar toast cronut cold-pressed truffaut. Gluten-free pickled leggings umami artisan cray. Copper mug ennui pork belly bushwick. Fashion axe intelligentsia vice photo booth la croix edison bulb enamel pin ennui marfa neutra cliche. Blog next level hella, quinoa mixtape XOXO prism activated charcoal occupy cloud bread readymade post-ironic schlitz tumeric.',
+      solutions: [
+        {
+          id: 0,
+          name: '',
+          description: '',
+          code: '',
+        },
+      ],
+      tests: [
+        {
+          id: 1,
+          funcWithArgs: 'maxValue([4, 7, 2, 8, 10, 9]); // -> 10',
+        },
+      ],
+    },
   ],
   current: {
     id: 0,
