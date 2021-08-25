27e673875f3cb1dafe1f8d1d5e385c6b6cf1fc71
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-24 20:19:54 -0700

    create and wire in sidebar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Heading, Flex, Link } from '@chakra-ui/react';
+import { Heading, Flex, Link, Button, useDisclosure } from '@chakra-ui/react';
+import Sidebar from './Sidebar';
 
 function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
   return (
     <Flex
       width="100%"
@@ -13,10 +16,16 @@ function NavBar() {
       shadow="sm"
       borderBottom="1px solid #C8C8C8"
     >
+      <Sidebar isOpen={isOpen} onClose={onClose} btnRef={btnRef} />
       <Link as={NavLink} to="/">
-        <Heading as="h1" size="lg" mt={[3, 0, 0, 0]} mr={[0, 3, 3, 3]}>
-          JS Space
-        </Heading>
+        <Flex>
+          <Heading as="h1" size="lg" mt={[3, 0, 0, 0]} mr={[0, 3, 3, 3]}>
+            JS Space
+          </Heading>
+          <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+            Problems
+          </Button>
+        </Flex>
       </Link>
       <Link as={NavLink} to="/login">
         <Heading as="h3" size="md" mt={[3, 0, 0, 0]} mr={[0, 3, 3, 3]}>
