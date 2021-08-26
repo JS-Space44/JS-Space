60af7166d0edec2461a188b8fe5ebce0ce729508
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-26 12:09:23 -0700

    render login logout in navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Heading, Flex, Link, Button, useDisclosure } from '@chakra-ui/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import {
+  Heading,
+  Flex,
+  Link,
+  Button,
+  Text,
+  useDisclosure,
+  IconButton,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import actions from '../actions/actions';
 import Sidebar from './Sidebar';
 
 function NavBar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const logoutUser = () => dispatch(actions.logoutUser());
   const btnRef = React.useRef();
+
   return (
     <Flex
       width="100%"
@@ -22,16 +38,30 @@ function NavBar() {
           <Heading as="h1" size="lg" mt={[3, 0, 0, 0]} mr={[0, 3, 3, 3]}>
             JS Space
           </Heading>
-          <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
-            Problems
-          </Button>
+
+          <Flex alignContent="center" justifyContent="center">
+            <Button
+              leftIcon={<HamburgerIcon />}
+              ml={2}
+              size="lg"
+              onClick={onOpen}
+              ref={btnRef}
+              variant="ghost"
+            >
+              Problems
+            </Button>
+          </Flex>
         </Flex>
       </Link>
-      <Link as={NavLink} to="/login">
-        <Heading as="h3" size="md" mt={[3, 0, 0, 0]} mr={[0, 3, 3, 3]}>
-          Temp Auth Link
-        </Heading>
-      </Link>
+      {!auth.isLoggedIn ? (
+        <Link as={NavLink} to="/login">
+          <Heading as="h3" size="md" mt={[3, 0, 0, 0]} mr={[0, 3, 3, 3]}>
+            Login
+          </Heading>
+        </Link>
+      ) : (
+        <Button onClick={logoutUser}>Logout</Button>
+      )}
     </Flex>
   );
 }
