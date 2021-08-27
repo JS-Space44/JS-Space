959d902e6662bb45191623fe7c143467b4c539dc
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-26 19:11:18 -0700

    comment out prism highlighting

--- client/components/Tests.jsx
+++ client/components/Tests.jsx
@@ -12,31 +12,31 @@ export default function Test({ currentProblem }) {
     }
   }, [currentProblem, tests]);
 
-  return (
-    <Box>
-      {tests &&
-        tests.map((test, index) => (
-          <Box className="language-js" key={`test-${index + 1}`} mb={6}>
-            <pre
-              className="language-js"
-              style={{ borderRadius: '4px', display: 'block' }}
-            >
-              <code className="language-js"> {test.funcWithArgs}</code>
-            </pre>
-          </Box>
-        ))}
-    </Box>
-  );
   // return (
   //   <Box>
   //     {tests &&
   //       tests.map((test, index) => (
-  //         <Box key={`test-${index + 1}`} mb={6}>
-  //           <pre style={{ borderRadius: '4px', display: 'block' }}>
-  //             <code> {test.funcWithArgs}</code>
+  //         <Box className="language-js" key={`test-${index + 1}`} mb={6}>
+  //           <pre
+  //             className="language-js"
+  //             style={{ borderRadius: '4px', display: 'block' }}
+  //           >
+  //             <code className="language-js"> {test.funcWithArgs}</code>
   //           </pre>
   //         </Box>
   //       ))}
   //   </Box>
   // );
+  return (
+    <Box>
+      {tests &&
+        tests.map((test, index) => (
+          <Box key={`test-${index + 1}`} mb={6}>
+            <pre style={{ borderRadius: '4px', display: 'block' }}>
+              <code> {test.funcWithArgs}</code>
+            </pre>
+          </Box>
+        ))}
+    </Box>
+  );
 }
