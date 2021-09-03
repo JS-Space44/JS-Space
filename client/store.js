038982dfa3167430285480e6f72f31a754451ab7
cjamesb <cjberry1234@sbcglobal.net>
2021-09-03 14:08:55 -0700

    sudocode

--- client/store.js
+++ client/store.js
@@ -3,6 +3,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+//redux store
 const store = createStore(
   reducers,
   composeWithDevTools(applyMiddleware(thunk))
